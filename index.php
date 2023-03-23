5a2d8ed7613e89367a82b1d67ba88ee93ae26a39
Anas <anas.mohammed@altd.in>
2023-03-23 13:20:45 +0530

    23/3/23

---
+++ index.php
@@ -0,0 +1,175 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/jquery.fullPage.css">
+    <link rel="stylesheet" href="./assets/css/common.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;1,200;1,400&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <title>ANAS MOHAMMED</title>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)
+      [0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-XXXXXXXX-X', 'auto') ; 
+
+      ga('send', 'pageview');
+    </script>
+  </head>
+
+  <body>
+    <header><a class="name" href="./index.html">Anaz Mohmd</a>
+      <div class="wrap">
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/anas-mohammed-1b64a2237/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+          <a href="https://www.instagram.com/_anazmohmed_/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://dribbble.com/anas_mohammd01?onboarding=true" target="_blank"><i class="fab fa-dribbble"></i></a>
+          <a href="https://codepen.io/anas-mohmd" target="_blank"><i class="fab fa-codepen"></i></a>
+        </div>
+        <div class="menuIcon js-menuBtn"></div>
+      </div>
+      <nav class="global-nav">
+        <ul class="global-nav__list">
+          <li><a href="./index.html">HOME</a></li>
+          <li><a href="./reile.html">WORKS</a></li>
+          <li><a href="./about.html">ABOUT ME</a></li>
+          <li><a class="js-contact" href="./#contact">CONTACT</a></li>
+          <li class="global-nav__sns"><a href="#" target="_blank"><i class="fab fa-twitter"></i></a><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+        </ul>
+      </nav>
+      <div class="curtain"></div>
+      <div class="loader"></div>
+    </header>
+    <main id="barba-wrapper">
+      <div class="barba-container" data-namespace="top">
+        <div class="fullpage" id="js-fullpage">
+          <div class="section">
+            <div class="fullpage__slide">
+              <div class="title title--top">
+                <h1 class="title__text js-letter">ANAZ<br>MOHMD</h1>
+                <div class="border js-letter"><span></span><span class="js-letter"></span></div>
+                <p class="title__lead js-letter">web designer / front end developer.</p>
+              </div>
+              <div class="moon">
+                <div class="moon__img js-parallax-moon">
+                  <div class="moon__front layer" data-depth="0.2">
+                    <div class="moon__text-wrap">
+                      <p class="moon__text js-moon" data-depth="0.5">PORTFOLIO</p>
+                    </div>
+                  </div>
+                  <div class="moon__front layer" data-depth="0.8">
+                    <div class="cloud cloud--front1 js-moon"><img src="./assets/img/cloud1.svg" alt="cloud"></div>
+                  </div>
+                  <div class="moon__back layer" data-depth="0.4">
+                    <div class="cloud cloud--back2 js-moon"><img src="./assets/img/b_cloud02.svg" alt="cloud"></div>
+                  </div>
+                  <div class="moon__front layer" data-depth="0.6">
+                    <div class="cloud cloud--front2 js-moon"><img src="./assets/img/cloud2.svg" alt="cloud"></div>
+                  </div>
+                  <div class="moon__back layer" data-depth="0.5"><img class="js-moon" src="./assets/img/moon.svg" alt="moon"></div>
+                  <div class="moon__front layer" data-depth="0.7">
+                    <div class="cloud cloud--front3 js-moon"><img src="./assets/img/cloud3.svg" alt="cloud"></div>
+                  </div>
+                  <div class="moon__back layer" data-depth="0.3">
+                    <div class="cloud cloud--back1 js-moon"><img src="./assets/img/b_cloud01.svg" alt="cloud"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="section">
+            <div class="fullpage__slide">
+              <div class="title">
+                <h2 class="title__text js-letter">Hair Space<br>REILE</h2>
+                <div class="border js-letter"><span></span><span class="js-letter"></span></div>
+                <p class="title__lead js-letter">website</p>
+                <div class="btn-wrap js-letter"><a class="btn" href="./reile.html">Show me more</a></div>
+              </div><a class="image image--works" href="/reile.html">
+                <div class="image__over">
+                  <div class="image__cover"></div>
+                  <div class="image__cover"></div>
+                </div>
+                <div class="page-num">
+                  <p>01</p>
+                </div></a>
+            </div>
+          </div>
+          <div class="section">
+            <div class="fullpage__slide">
+              <div class="title">
+                <h2 class="title__text js-letter">About Me</h2>
+                <div class="border js-letter"><span></span><span class="js-letter"></span></div>
+                <p class="title__lead js-letter">I love Design, Technology,<br>and Story.</p>
+                <div class="btn-wrap js-letter"><a class="btn" href="./about.html">Show me more</a></div>
+              </div><a class="image image--about" href="./about.html">
+                <div class="image__over">
+                  <div class="image__cover"></div>
+                  <div class="image__cover"></div>
+                </div>
+                <div class="page-num">
+                  <p>02</p>
+                </div></a>
+            </div>
+          </div>
+          <div class="section">
+            <div class="fullpage__slide">
+              <div class="title">
+                <h2 class="title__text js-letter">Get In Touch</h2>
+                <div class="border js-letter"><span></span><span class="js-letter"></span></div>
+                <ul>
+                  <li class="js-letter"><a href="mailto:fully2fizz@gmail.com">fully2fizz@gmail.com</a></li>
+                  <li class="js-letter"><a href="tel:+91 9656800668" target="_blank">+91 9656800668</a></li>
+                  <li class="js-letter">
+                    <a href="https://www.linkedin.com/in/anas-mohammed-1b64a2237/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.instagram.com/_anazmohmed_/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a href="https://dribbble.com/anas_mohammd01?onboarding=true" target="_blank"><i class="fab fa-dribbble"></i></a>
+                    <a href="https://codepen.io/anas-mohmd" target="_blank"><i class="fab fa-codepen"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="image image--contact">
+                <div class="image__over">
+                  <div class="image__cover"></div>
+                  <div class="image__cover"></div>
+                </div>
+                <div class="page-num">
+                  <p>03</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="scrollDown">SCROLLDOWN</p>
+        <div class="moon-background">
+          <div class="moonlight">
+            <div class="moonlight__wrap js-parallax-moonlight">
+              <div class="layer moonlight__img" data-depth="0.2"></div>
+            </div>
+          </div>
+        </div>
+        <div class="star js-parallax-star">
+          <div class="layer" data-depth="0.1">
+            <div class="star__img"><img src="./assets/img/star.svg"></div>
+          </div>
+        </div>
+        <div class="sky-color"></div>
+      </div>
+    </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./assets/js/jquery.fullPage.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.min.js"></script>
+    <script src="./assets/js/parallax.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/barba.js/1.0.0/barba.min.js"></script>
+    <script src="./assets/js/common.min.js"></script>
+  </body>
+</html>
